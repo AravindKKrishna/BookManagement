c66a3a0620a585347df1e1255eca3756d5c21db5
Aravind K Krishna <ananthkkrishna2001@gmail.com>
2024-10-01 20:46:16 +0530

    inital setup done

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const {users}=require("./data/users.json")
+const app = express();
+const PORT = 8081;
+app.use(express.json());
+
+app.get("/users",(req,res)=>{
+  res.status(200).json({
+    success: true,
+    data: users,
+  });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "server is up and running :-)",
+  });
+});
+app.get("#", (req, res) => {
+  res.status(404).json({
+    message: "This server does not exist",
+  });
+});
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
